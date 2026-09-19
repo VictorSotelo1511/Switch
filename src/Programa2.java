9d7e765d092d607994ea5c091cf2dada4f5edd67
VictorSotelo1511 <victorsotelo1511@gmail.com>
2026-09-19 15:45:28 -0600

    primer commit

---
+++ src/Programa2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Programa2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese el primer número: ");
+        double numero1 = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.print("Ingrese el segundo número: ");
+        double numero2 = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.println("Ingrese la operacion que desea realizar: ");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicacion");
+        System.out.println("4. Division");
+        int operacion = sc.nextInt();
+
+        switch (operacion) {
+            case 1:
+                System.out.println("Resultado: " + (numero1 + numero2));
+                break;
+            case 2:
+                System.out.println("Resultado: " + (numero1 - numero2));
+                break;
+            case 3:
+                System.out.println("Resultado: " + (numero1 * numero2));
+                break;
+            case 4:
+                if (numero2 == 0) {
+                    System.out.println("No se puede dividir por cero");
+                } else {
+                    System.out.println("Resultado: " + (numero1 / numero2));
+                }
+                break;
+
+            default:
+                System.out.println("Número no válido");
+        }
+    }
+}
---
+++ src/Programa3.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Programa3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int saldo = 1000;
+
+        System.out.print("Ingrese una opcion: ");
+        System.out.println("1. Verficar Saldo");
+        System.out.println("2. Depositar Dinero");
+        System.out.println("3. Retirar Dinero");
+        System.out.println("4. Salir");
+        int operacion = sc.nextInt();
+
+        switch (operacion) {
+            case 1:
+                System.out.println("Saldo: " + saldo);
+                break;
+            case 2:
+                System.out.print("Ingrese el monto a depositar: ");
+                int montoDeposito = sc.nextInt();
+                saldo += montoDeposito;
+                System.out.println("Depósito realizado. Nuevo saldo: " + saldo);
+                break;
+            case 3:
+                System.out.print("Ingrese el monto a retirar: ");
+                int montoRetiro = sc.nextInt();
+                if (montoRetiro <= saldo) {
+                    saldo -= montoRetiro;
+                    System.out.println("Retiro realizado. Nuevo saldo: " + saldo);
+                } else {
+                    System.out.println("Fondos insuficientes");
+                }
+                break;
+            case 4:
+                System.out.println("Saliendo del sistema...");
+                break;
+            default:
+                System.out.println("Número no válido");
+        }
+    }
+}
